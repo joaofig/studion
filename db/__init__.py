2cf8be7da1ad0f4dae3171ca377b9d6e18763e76
joaofig <joao.figueira@gmail.com>
2026-05-03 11:29:11 +0100

    Add study management utilities to database module

--- db/__init__.py
+++ db/__init__.py
@@ -1,9 +1,15 @@
 from .config import DatabaseConfig, load_database_config
 from .sqlite import get_connection, initialize_database
+from .study import create_study, delete_study, get_all_studies, get_study, update_study
 
 __all__ = [
     "DatabaseConfig",
     "load_database_config",
     "get_connection",
     "initialize_database",
+    "get_all_studies",
+    "get_study",
+    "create_study",
+    "update_study",
+    "delete_study",
 ]
